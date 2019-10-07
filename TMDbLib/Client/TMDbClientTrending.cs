5abe3f3880e5283fb56251250985816c17e3e1c3
Amgad Awad <amgad.awad@gmail.com>
2019-10-07 14:30:20 +0300

    add paging to trending search

--- TMDbLib/Client/TMDbClientTrending.cs
+++ TMDbLib/Client/TMDbClientTrending.cs
@@ -14,31 +14,53 @@ namespace TMDbLib.Client
 {
     public partial class TMDbClient
     {
-        public async Task<SearchContainer<SearchMovie>> GetTrendingMoviesAsync(TimeWindow timeWindow, CancellationToken cancellationToken = default(CancellationToken))
+        /// <summary>
+        /// Get Trending Movies
+        /// </summary>
+        /// <param name="timeWindow"></param>
+        /// <param name="page"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public async Task<SearchContainer<SearchMovie>> GetTrendingMoviesAsync(TimeWindow timeWindow, int page = 0, CancellationToken cancellationToken = default(CancellationToken))
         {
             RestRequest req = _client.Create("trending/movie/{time_window}");
             req.AddUrlSegment("time_window", timeWindow.ToString());
-
+            if (page > 0)
+                req.AddQueryString("page", page.ToString());
             RestResponse<SearchContainer<SearchMovie>> resp = await req.ExecuteGet<SearchContainer<SearchMovie>>(cancellationToken).ConfigureAwait(false);
 
             return resp;
         }
-
-        public async Task<SearchContainer<SearchTv>> GetTrendingTvAsync(TimeWindow timeWindow, CancellationToken cancellationToken = default(CancellationToken))
+        /// <summary>
+        /// Get Trending tv series
+        /// </summary>
+        /// <param name="timeWindow"></param>
+        /// <param name="page"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public async Task<SearchContainer<SearchTv>> GetTrendingTvAsync(TimeWindow timeWindow, int page = 0, CancellationToken cancellationToken = default(CancellationToken))
         {
             RestRequest req = _client.Create("trending/tv/{time_window}");
             req.AddUrlSegment("time_window", timeWindow.ToString());
-
+            if (page > 0)
+                req.AddQueryString("page", page.ToString());
             RestResponse<SearchContainer<SearchTv>> resp = await req.ExecuteGet<SearchContainer<SearchTv>>(cancellationToken).ConfigureAwait(false);
 
             return resp;
         }
-
-        public async Task<SearchContainer<SearchPerson>> GetTrendingPeopleAsync(TimeWindow timeWindow, CancellationToken cancellationToken = default(CancellationToken))
+        /// <summary>
+        /// Get trending People
+        /// </summary>
+        /// <param name="timeWindow"></param>
+        /// <param name="page"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public async Task<SearchContainer<SearchPerson>> GetTrendingPeopleAsync(TimeWindow timeWindow, int page = 0, CancellationToken cancellationToken = default(CancellationToken))
         {
             RestRequest req = _client.Create("trending/person/{time_window}");
             req.AddUrlSegment("time_window", timeWindow.ToString());
-
+            if (page > 0)
+                req.AddQueryString("page", page.ToString());
             RestResponse<SearchContainer<SearchPerson>> resp = await req.ExecuteGet<SearchContainer<SearchPerson>>(cancellationToken).ConfigureAwait(false);
 
             return resp;
